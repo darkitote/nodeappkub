2422d7e388e45d92ba0ba58b9b67a9fa37f4708d
darkitote <darckgamer_2002@hotmail.com>
2025-02-12 15:41:36 -0600

    puerto

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hooooolaa" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Roblox" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " ALgo bien si jala" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
